252437444738a3f00528aff418c4e6a3dc8464c4
Tauses <dettavseunivers@gmail.com>
2024-03-18 22:04:19 +0100

    Added GUI event click

--- src/main/java/com/taus/huskhomes_gui/Application/Model/Main.java
+++ src/main/java/com/taus/huskhomes_gui/Application/Model/Main.java
@@ -3,7 +3,9 @@ package com.taus.huskhomes_gui.Application.Model;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -26,18 +28,40 @@ public final class Main extends JavaPlugin implements Listener {
     }
 
     public void openGUI(Player player) {
-        Inventory GUI = Bukkit.createInventory(null,9, "Homes");
+        Inventory GUI = Bukkit.createInventory(null, 9, "Homes");
 
         ItemStack item1 = new ItemStack(Material.DARK_OAK_DOOR);
         ItemStack item2 = new ItemStack(Material.CHERRY_DOOR);
 
         GUI.setItem(2, item1);
-        GUI.setItem(5,item2);
+        GUI.setItem(5, item2);
 
         player.openInventory(GUI);
 
 
     }
 
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        if (!e.getView().getTitle().equals("Homes")) {
+            return;
+        }
+        if (e.getCurrentItem() == null || e.getCurrentItem().getType() == Material.AIR) {
+            return;
+        }
+        if (!(e.getWhoClicked() instanceof Player)) {
+            return;
+        }
+        Player player = (Player) e.getWhoClicked();
+
+        if (e.getRawSlot() == 2) {
+            player.sendMessage("set a home");
+        } else if (e.getRawSlot() == 5) {
+            player.sendMessage("Set another home");
+        }
+
+        player.closeInventory();
+
+    }
 
 }
